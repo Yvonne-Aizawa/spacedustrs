ac7f6ad757d8a084ec74dfbf70f4bf2f2d4cb817
Yvonne-Aizawa <106619470+Yvonne-Aizawa@users.noreply.github.com>
2023-05-21 15:35:58 +0200

    Update factions_api.rs

--- src/tests/factions_api.rs
+++ src/tests/factions_api.rs
@@ -3,7 +3,7 @@
 mod tests {
     use crate::models::register_request::{Faction, RegisterRequest};
     use crate::apis::default_api::register;
-    use crate::apis::factions_api::get_factions;
+    use crate::apis::factions_api::{get_faction, get_factions};
     use crate::apis::configuration::Configuration;
     use tokio::fs::File;
     use tokio::io::AsyncWriteExt;
@@ -14,8 +14,16 @@ mod tests {
     async fn test_get_faction(){
         let mut configuration = Configuration::new();
         configuration.bearer_access_token = Some(GLOBAL_AAC.to_string());
-        let factions = get_factions(&configuration, Some(1), Some(20)).await;
-        assert!(factions.is_ok());
+        let faction = get_faction(&configuration, "COSMIC").await;
+        assert!(faction.is_ok());
  
     }
+    #[tokio::test]
+    async fn test_get_factions(){
+        let mut configuration = Configuration::new();
+        configuration.bearer_access_token = Some(GLOBAL_AAC.to_string());
+        let factions = get_factions(&configuration, Some(1), Some(20)).await;
+        assert!(factions.is_ok());
+    }
+
 }
